43c5360342c6c29f253bf29225676a4844ae1890
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-11-25 19:08:48 +0300

    api v2 endpoints update profile

--- backend/api_v2/profiles/views.py
+++ backend/api_v2/profiles/views.py
@@ -20,10 +20,31 @@ async def get_candidate_profile(
     return await crud.get_profile(session=session, payload=payload)
 
 
-@router.put("/edit/")
+@router.put("/update/", response_model=PutCandidateProfile)
 async def update_profile(
     data_in: PutCandidateProfile,
     payload: dict = Depends(get_current_token_payload),
     session: AsyncSession = Depends(get_db),
 ):
     return await crud.update_profile(data_in=data_in, session=session, payload=payload)
+
+@router.get("/skills/")
+async def get_user_skills(
+    payload: dict = Depends(get_current_token_payload),
+    session: AsyncSession = Depends(get_db),
+):
+    pass
+
+@router.put("/skills/add/")
+async def add_skill_for_user(
+    payload: dict = Depends(get_current_token_payload),
+    session: AsyncSession = Depends(get_db),
+):
+    pass
+
+@router.delete("/skills/remove/")
+async def remove_skill_from_user(
+    payload: dict = Depends(get_current_token_payload),
+    session: AsyncSession = Depends(get_db),
+):
+    pass
