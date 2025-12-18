14dd9ef8c4c7d46ac2193f546293f91e880fddd3
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-12-18 21:53:42 +0300

    created at field

--- app/shared/infrastructure/base.py
+++ app/shared/infrastructure/base.py
@@ -1,10 +1,16 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
-
+from sqlalchemy import DateTime, func
+from datetime import datetime
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
